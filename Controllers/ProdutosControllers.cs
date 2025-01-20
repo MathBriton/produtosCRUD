e6a2e194c1a63b2a649b6dcb805076fbc1c9ab27
MathBriton <mathhbrito@gmail.com>
2025-01-20 13:48:48 -0300

    primeiro commit

---
+++ Controllers/ProdutosControllers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace produtoCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosControllers : ControllerBase
+    {
+    }
+}
